6ff62b4711e85a230841783bbdf8a5855781e97a
sera Pak <100737771+hiereit-dev@users.noreply.github.com>
2025-04-16 17:34:04 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.55 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -5,11 +5,5 @@ func solution(_ A:[Int], _ B:[Int]) -> Int
     var a = A.sorted()
     var b = B.sorted {$0 > $1}
     
-    var answer1 = zip(a, b).map {$0.0 * $0.1}.reduce(0, +)
-    
-    a = A.sorted {$0 > $1}
-    b = B.sorted()
-    var answer2 = zip(a, b).map {$0.0 * $0.1}.reduce(0, +)
-
-    return min(answer1, answer2)
+    return zip(a, b).map {$0.0 * $0.1}.reduce(0, +)
 }
